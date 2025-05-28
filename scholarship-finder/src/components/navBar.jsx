914dbd5705038e71fe12cb0532bd16a6351060d0
Aditya Sahu <awesomeadi457@gmail.com>
2025-05-28 12:17:07 +0530

    Update navBar.jsx

--- scholarship-finder/src/components/navBar.jsx
+++ scholarship-finder/src/components/navBar.jsx
@@ -3,6 +3,7 @@ import { useNavigate, useLocation } from "react-router-dom";
 
 export default function NavBar() {
     const navigate = useNavigate();
+    const location = useLocation();
 
     const isActive = (path) => location.pathname === path;
 
